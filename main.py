b8317f374ba346783e7993e334139ba4ef86ce97
timbaginski <timbaginski3@gmail.com>
2022-02-19 15:48:12 -0500

    main updated to add study manager

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+from re import S
 import sys
 import argparse
 import pdf_parser
-import studymanager
+from studymanager import StudyManager
 
 
 def main():
     parser = argparse.ArgumentParser(description="create a study set")
-    parser.add_argument('-t', action='store_const', const=True, 
+    parser.add_argument('-t', dest='answer_with_term', action='store_const', const=True, default=False,
     help='Answer with term instead of definition')
     parser.add_argument('path', help='path of study sheet PDF')
 
     args = parser.parse_args()
+    lines = pdf_parser.parse_pdf(args.path)
+    print(args.answer_with_term)
+    study_manager = StudyManager(lines, args.answer_with_term)
 
 
 main()
