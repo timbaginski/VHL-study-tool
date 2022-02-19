bc9c79bd74e3f6b2062384c6d51e2a4e6d8cfd5d
timbaginski <timbaginski3@gmail.com>
2022-02-19 15:26:41 -0500

    Added argparse to main

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import sys
+import argparse
 import pdf_parser
 import studymanager
 
+
+def main():
+    parser = argparse.ArgumentParser(description="create a study set")
+    parser.add_argument('-t', action='store_const', const=True, 
+    help='Answer with term instead of definition')
+    parser.add_argument('path', help='path of study sheet PDF')
+
+    args = parser.parse_args()
+
+
+main()
